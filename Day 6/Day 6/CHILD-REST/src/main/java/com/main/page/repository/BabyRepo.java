cd0ab2b1cf1f5adfda0c80340593e5c16520bb6e
Harii <126044413+vynter@users.noreply.github.com>
2023-02-28 19:50:00 +0530

    Delete Day 6/Day 6 directory

--- Day 6/Day 6/CHILD-REST/src/main/java/com/main/page/repository/BabyRepo.java
+++
@@ -1,9 +0,0 @@
-package com.main.page.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.main.page.model.Baby;
-
-public interface BabyRepo extends JpaRepository<Baby,Integer>{
-
-}
